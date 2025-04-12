d95c16a77b3ebd290430c89a9cc84d4e03ac408c
start.vaadin.com <no-reply@vaadin.com>
2025-04-12 17:03:04 +0000

    Generated project

---
+++ src/main/java/org/dasher/speed/base/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable domain classes.
+ */
+@NullMarked
+package org.dasher.speed.base.domain;
+
+import org.jspecify.annotations.NullMarked;
---
+++ src/main/java/org/dasher/speed/base/ui/component/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package contains reusable UI components.
+ */
+@NullMarked
+package org.dasher.speed.base.ui.component;
+
+import org.jspecify.annotations.NullMarked;
