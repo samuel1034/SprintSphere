bce4e3150577f8f61b3eb85459cc9646bd59f434
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2025-04-14 11:52:50 -0400

    Project Service Created

---
+++ src/main/java/org/dasher/speed/project/repository/ProjectRepository.java
@@ -0,0 +1,5 @@
+package org.dasher.speed.project.repository;
+
+public interface ProjectRepository {
+
+}
--- src/main/java/org/dasher/speed/project/service/ProjectService.java
+++ src/main/java/org/dasher/speed/project/service/ProjectService.java
@@ -1,11 +1,55 @@
 package org.dasher.speed.project.service;
 
 
+import org.dasher.speed.project.model.Project;
+import org.dasher.speed.project.model.enums.ProjectPhase;
+import org.dasher.speed.project.repository.ProjectRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ProjectService {
 
-    public name
+    private final ProjectRepository projectRepository;
+
+    public ProjectService (ProjectRepository projectRepository) {
+
+        this.projectRepository = projectRepository;
+    }
+
+    public List<Project> findAll () {
+        return projectRepository.findAll();
+    }
+
+    public Optional <Project> findById (Long id){
+        return projectRepository.findById(id);
+    }
+
+    public Project save (Project project) {
+        return projectRepository.save(project);
+    }
+
+    public void Delete (Project project) {
+        projectRepository.delete(project);
+    }
+
+    public Map <ProjectPhase, Long> countProjectsByPhase(){
+        return projectRepository.findAll()
+                .stream()
+                .collect(Collectors.groupingBy(
+                        Project::getPhase,
+                        Collectors.counting()
+                ));
+    }
+
+    public List <Project> findByPhase (ProjectPhase phase) {
+        return projectRepository.findByPhase(phase);
+    }
+
+
 
 }
