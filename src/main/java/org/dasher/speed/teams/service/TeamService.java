4bbbd27d16724eed854a48faa1bd5c20fd635652
Samuel Fuentes (Genius) <samuelgenius1034@gmail.com>
2025-04-15 14:53:42 -0400

    Created the Team Service

--- src/main/java/org/dasher/speed/teams/service/TeamService.java
+++ src/main/java/org/dasher/speed/teams/service/TeamService.java
@@ -1,8 +1,33 @@
 package org.dasher.speed.teams.service;
 
+import org.dasher.speed.project.model.Team;
+import org.dasher.speed.teams.repository.TeamRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TeamService {
+    private final TeamRepository teamRepository;
 
+    public TeamService (TeamRepository teamRepository) {
+        this.teamRepository = teamRepository;
+    }
+
+    public List <Team> findAll () {
+        return teamRepository.findAll();
+    }
+
+    public Team save (Team team) {
+        return teamRepository.save(team);
+    }
+
+    public Optional<Team> findById (Long id) {
+        return teamRepository.findById(id);
+    }
+
+    public void delete (Team team) {
+        teamRepository.delete(team);
+    }
 }
